a2231ff81cd8c5ae7e6ec95660a349dd13374f9d
Zabir Al Nazi Nabil <zabiralnazi@codeassign.com>
2017-08-13 02:41:57 +0600

    NT

---
+++ Week 1 with Number Theory/Miller_Rabin.cpp
@@ -0,0 +1,60 @@
+//Miller-Rabin primality testing
+//Miller(prime_to_test, iterations)
+//probability of failing: (1/4)^iterations
+#include<bits/stdc++.h>
+using namespace std;
+/* This function calculates (a^b)%c */
+long long modulo(long long a,long long b,long long c){
+    long long x=1,y=a; // long long is taken to avoid overflow of intermediate results
+    while(b > 0){
+        if(b%2 == 1){
+            x=(x*y)%c;
+        }
+        y = (y*y)%c; // squaring the base
+        b /= 2;
+    }
+    return x%c;
+}
+/* this function calculates (a*b)%c taking into account that a*b might overflow */
+long long mulmod(long long a,long long b,long long c){
+    long long x = 0,y=a%c;
+    while(b > 0){
+        if(b%2 == 1){
+            x = (x+y)%c;
+        }
+        y = (y*2)%c;
+        b /= 2;
+    }
+    return x%c;
+}
+/* Miller-Rabin primality test, iteration signifies the accuracy of the test */
+bool Miller(long long p,int iteration){
+    if(p<2){
+        return false;
+    }
+    if(p!=2 && p%2==0){
+        return false;
+    }
+    long long s=p-1;
+    while(s%2==0){
+        s/=2;
+    }
+    for(int i=0;i<iteration;i++){
+        long long a=rand()%(p-1)+1,temp=s;
+        long long mod=modulo(a,temp,p);
+        while(temp!=p-1 && mod!=1 && mod!=p-1){
+            mod=mulmod(mod,mod,p);
+            temp *= 2;
+        }
+        if(mod!=p-1 && temp%2==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main()
+{
+    cout<<Miller(1e9+7,1);
+    return 0;
+}
